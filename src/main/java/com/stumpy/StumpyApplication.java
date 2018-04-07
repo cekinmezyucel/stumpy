0f80ed40ad8b0ea1723fa6ceb87fcae2db98b20a
Yucel Cekinmez <cekinmezyucel@gmail.com>
2018-04-07 12:14:29 +0300

    initial

---
+++ src/main/java/com/stumpy/StumpyApplication.java
@@ -0,0 +1,12 @@
+package com.stumpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StumpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StumpyApplication.class, args);
+	}
+}
