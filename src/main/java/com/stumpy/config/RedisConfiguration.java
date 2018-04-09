33bae639e52edad174d4c3322f63075b116b0468
Yucel Cekinmez <cekinmezyucel@gmail.com>
2018-04-10 02:32:29 +0300

    RedisStandaloneConfiguration is used

--- src/main/java/com/stumpy/config/RedisConfiguration.java
+++ src/main/java/com/stumpy/config/RedisConfiguration.java
@@ -2,6 +2,7 @@ package com.stumpy.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
@@ -12,9 +13,7 @@ public class RedisConfiguration {
 
   @Bean
   JedisConnectionFactory getJedisConnectionFactory() {
-    JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-    jedisConnectionFactory.setHostName("localhost");
-    jedisConnectionFactory.setPort(6379);
+    JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(new RedisStandaloneConfiguration());
     return jedisConnectionFactory;
   }
 
