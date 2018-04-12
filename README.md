7f12a844d450cf07f6e4efd92095d43c34673caa
Yucel Cekinmez <cekinmezyucel@gmail.com>
2018-04-12 17:47:17 +0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# stumpy
-This is a URL Shortener Application.
+# Stumpy
+A URL Shortener Application.
+
+## Objective
+The main objective of this project; build a complete application with various technologies and approaches.
+
+### Tech
+Stumpy developed with Java 8, a number of technologies and open source projects:
+
+* [SpringBoot]
+* [Redis]
+* [Docker]
+* [Maven]
+* [JUnit]
+* [Swagger]
+* [Asciidoctor]
+* [Swagger2Markup]
+* [Apache Maven Checkstyle Plugin]
+
+[SpringBoot]: <https://projects.spring.io/spring-boot/>
+[Redis]: <https://redis.io/>
+[Docker]: <https://www.docker.com/>
+[JUnit]: <https://junit.org/>
+[Swagger]: <https://swagger.io/>
+[Asciidoctor]: <https://asciidoctor.org/>
+[Swagger2Markup]: <https://github.com/Swagger2Markup/swagger2markup>
+[Maven]: <https://maven.apache.org/>
+[Apache Maven Checkstyle Plugin]: <https://maven.apache.org/plugins/maven-checkstyle-plugin/>
--- src/main/java/com/stumpy/localization/SwaggerLocalizations.java
+++ src/main/java/com/stumpy/localization/SwaggerLocalizations.java
@@ -8,13 +8,13 @@ public class SwaggerLocalizations {
 
   public static final String STANDART_PROD_CONS = "application/json, application/xml";
 
-  public static final String SHORTENER_GET_PATH = "#{SHORTENER_GET_PATH}";
-  public static final String SHORTENER_GET_NAME = "#{SHORTENER_GET_NAME}";
-  public static final String SHORTENER_GET_DESCRIPTION = "#{SHORTENER_GET_DESCRIPTION}";
-  public static final String SHORTENER_REQUEST = "#{SHORTENER_REQUEST}";
-  public static final String SHORTENER_RESPONSE = "#{SHORTENER_RESPONSE}";
+  public static final String SHORTENER_GET_PATH = "/shortener/get";
+  public static final String SHORTENER_GET_NAME = "Shortener Get Service";
+  public static final String SHORTENER_GET_DESCRIPTION = "This service returns short URL";
+  public static final String SHORTENER_REQUEST = "Shortener Get Request";
+  public static final String SHORTENER_RESPONSE = "Shortener Get Response";
 
-  public static final String LONG_URL = "#{LONG_URL}";
-  public static final String SHORT_URL = "#{SHORT_URL}";
+  public static final String LONG_URL = "Long URL";
+  public static final String SHORT_URL = "Short URL";
 
 }
