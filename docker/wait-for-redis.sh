14b1acb31d800310f23971d3598dd864214454d9
Yucel Cekinmez <cekinmezyucel@gmail.com>
2018-04-14 02:33:00 +0300

    remove non used sh. solved with depends_on

--- docker/wait-for-redis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-while ! nc -z redis 6379 ; do
-    echo "Waiting for upcoming Redis"
-    sleep 1
-done
-
-java -jar -Dspring.profiles.active=prod /opt/stumpy-service/lib/stumpy-service.jar
