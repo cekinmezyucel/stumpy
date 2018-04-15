720d0efe6bc1f149e8e790964547e04e9d0e15e3
Yucel Cekinmez <cekinmezyucel@gmail.com>
2018-04-15 14:03:24 +0300

    fix

--- src/test/java/com/stumpy/StumpyApplicationTests.java
+++ src/test/java/com/stumpy/StumpyApplicationTests.java
@@ -2,13 +2,12 @@ package com.stumpy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource(locations = "classpath:test.properties")
-@SpringBootTest
+// @SpringBootTest
 public class StumpyApplicationTests {
 
   @Test
