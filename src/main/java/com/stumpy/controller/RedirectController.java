810ab72007390dcd6262e9e74877fa284c3bb118
Yucel Cekinmez <cekinmezyucel@gmail.com>
2018-10-24 23:44:15 +0100

    Get requests changed RequestMapping to GetMapping

--- src/main/java/com/stumpy/controller/RedirectController.java
+++ src/main/java/com/stumpy/controller/RedirectController.java
@@ -1,14 +1,13 @@
 package com.stumpy.controller;
 
 import static org.springframework.http.HttpStatus.TEMPORARY_REDIRECT;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Context;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +31,7 @@ public class RedirectController {
    * @param httpServletResponse.
    */
   @ResponseStatus(TEMPORARY_REDIRECT)
-  @RequestMapping(value = "/{shortUrl}", method = GET)
+  @GetMapping(value = "/{shortUrl}")
   public void redirect(@PathVariable String shortUrl, @Context HttpServletResponse httpServletResponse) {
     String longUrl = redirectService.getLongUrl(shortUrl);
     httpServletResponse.setHeader("Location", longUrl);
--- src/main/java/com/stumpy/controller/StatisticController.java
+++ src/main/java/com/stumpy/controller/StatisticController.java
@@ -2,11 +2,11 @@ package com.stumpy.controller;
 
 import static com.stumpy.localization.SwaggerLocalizations.STANDART_PROD_CONS;
 import static org.springframework.http.HttpStatus.OK;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import io.swagger.annotations.Api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,7 +24,7 @@ public class StatisticController {
   private StatisticService statisticService;
 
   @ResponseStatus(OK)
-  @RequestMapping(value = "/count/{statisticTypeName}/{shortUrl}", method = GET)
+  @GetMapping(value = "/count/{statisticTypeName}/{shortUrl}")
   public Long getHitCount(@PathVariable("statisticTypeName") String statisticTypeName,
       @PathVariable("shortUrl") String shortUrl) {
     return statisticService.getStatisticCount(shortUrl, statisticTypeName);
